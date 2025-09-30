392a96229f9f6214246e5b74565242e670e3935e
Khoa Nguyen <thaomanh2023@gmail.com>
2025-09-30 20:42:58 +0700

    Add project files.

---
+++ do an 2/Controllers/ShopController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace do_an_2.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult Phone()
+        {
+            return View();
+        }
+        public ActionResult Laptop()
+        {
+            return View();
+        }
+        public ActionResult Headphone()
+        {
+            return View();
+        }
+        public ActionResult Smartwatch() 
+        {
+            return View();
+        }
+
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+        public ActionResult ChiTietSP()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+    }
+}
